3229aefdf0a058e75b231439418d6e60dcb9370b
wangwu <wangwu@ngarihealth.com>
2019-03-05 22:31:50 +0800

    代码修改

--- src/main/java/com/wwclr/controller/entity/student.java
+++ src/main/java/com/wwclr/controller/entity/student.java
@@ -1,7 +1,48 @@
 package com.wwclr.controller.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * Created by wwclr on 2019/3/4.
  */
-public class student {
+public class Student {
+    @JsonIgnore
+    private String userId;
+    private String name;
+    @JsonInclude
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String userId, String name, Integer age) {
+        this.userId = userId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/wwclr/controller/server/helloController.java
+++ src/main/java/com/wwclr/controller/server/helloController.java
@@ -1,15 +1,23 @@
 package com.wwclr.controller.server;
 
+import com.wwclr.entity.Student;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by wwclr on 2019/3/4.
  */
-@RestController
+//@Controller
+@RestController  //这个=controller+responsebody
+@RequestMapping("/student")
 public class helloController {
-    @RequestMapping("/")
-    public Object sayHello(){
-        return "hello spring haha";
+    @RequestMapping("/hello")
+//    @ResponseBody
+    public Student sayHello(){
+        Student student=new Student("123","wangwu",26);
+       return student;
+
     }
 }
