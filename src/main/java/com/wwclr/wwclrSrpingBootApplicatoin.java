3af257bf1de782ce9b16354b103f75e17dfc5e79
wangwu <wangwu@ngarihealth.com>
2019-03-05 22:41:50 +0800

    代码修改

--- src/main/java/com/wwclr/wwclrSrpingBootApplicatoin.java
+++ src/main/java/com/wwclr/wwclrSrpingBootApplicatoin.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by wwclr on 2019/3/4.
  */
 @SpringBootApplication
+
 public class wwclrSrpingBootApplicatoin {
     public static void main(String[] args) {
         SpringApplication.run(wwclrSrpingBootApplicatoin.class, args);
